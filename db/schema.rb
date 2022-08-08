20aee159ab26e7c7adf60bf73650b1549ac16dff
thonght-3051 <hoang.trung.thong@sun-asterisk.com>
2022-08-08 17:27:27 +0700

    Task 51858 make migartion and model

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2022_08_08_102634) do
   create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.integer "type", default: 0
+    t.integer "type"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -89,11 +90,19 @@ ActiveRecord::Schema.define(version: 2022_08_08_102634) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
+<<<<<<< HEAD
     t.string "phone"
     t.string "email"
     t.string "password_digest"
     t.integer "role", default: 1, comment: "1: 'user', 2: 'admin'"
     t.integer "status", default: 1, comment: "1: 'active', 2: 'block'"
+=======
+    t.integer "phone"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role"
+    t.integer "status"
+>>>>>>> Task 51858 make migartion and model
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
